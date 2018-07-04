be23bfefe017e9a7180c329e7e1a85c5434f0ca9
ABOUFARID Salah <salah.aboufarid@gmail.com>
2018-07-04 16:44:39 +0200

    Retrieve movies by type

--- routes/moviesRoute.js
+++ routes/moviesRoute.js
@@ -27,4 +27,21 @@ router.get("/api/search", function(req, res) {
     });
 });
 
+router.get("/api/movies/:type", function(req, res) {
+  let { type } = req.params;
+  let { p } = req.query;
+  axios
+    .get(
+      `https://api.themoviedb.org/3/movie/${type}?api_key=${
+        process.env.THEMOVIEDB_KEY
+      }&language=fr-FR&page=${p}`
+    )
+    .then(function(response) {
+      res.json(response.data);
+    })
+    .catch(function(error) {
+      res.json(getJsonErr(error));
+    });
+});
+
 module.exports = router;
